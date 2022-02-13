8c5688b5a2cad27642726f39293beb53518327aa
PrishaMadane <70759166+PrishaMadane@users.noreply.github.com>
2022-02-13 10:37:28 +0530

    Update sketch.js

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -11,7 +11,7 @@ function draw() {
   background(0);  
 
   hr= hour();
-  mn= mintue();
+  mn= minute();
   sc= second();
 
    translate(200,200);
@@ -27,6 +27,35 @@ function draw() {
    strokeWeight(7);
    line(0,0,100,0);
    pop();
+
+   push();
+   rotate(mnAngle);
+   stroke("blue");
+   strokeWeight(7);
+   line(0,0,100,0);
+   pop();
+   
+   push();
+   rotate(hrAngle);
+   stroke("green");
+   strokeWeight(7);
+   line(0,0,100,0);
+   pop();
+
+   noFill();
+   stroke("red");
+   strokeWeight(7);
+   arc(0,0,300,300,0,scAngle);
+
+   noFill();
+   stroke("blue");
+   strokeWeight(7);
+   arc(0,0,280,280,0,mnAngle);
+
+   noFill();
+   stroke("green");
+   strokeWeight(7);
+   arc(0,0,260,260,0,hrAngle);
    
   drawSprites();
 }
